11808606261cef5f54583ea52b77bccdd0fb6c52
ABDULHADI JABER ALWAN ALSHEHRI <1847417@stu.kau.edu.sa>
2023-01-24 11:12:19 +0300

    added to poem and added testing

--- src/test/java/org/example/KalculatorTest.java
+++ src/test/java/org/example/KalculatorTest.java
@@ -1,39 +1,19 @@
 package org.example;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * Unit test for simple App.
  */
 public class KalculatorTest
 
-    extends TestCase
-
 {
     private final Kalculator Kal = new Kalculator();
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public KalculatorTest(String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( KalculatorTest.class );
-    }
-
 
     @org.junit.Test
-   void testApp()
+   public void testApp()
     {
      assertEquals(false,Kal.isLetter("5"));
         assertEquals(true,Kal.isLetter("a"));
